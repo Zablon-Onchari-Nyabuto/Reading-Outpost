e71386b9c3a5b9ec6af3180c64f354de818a0367
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-08-31 12:08:19 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const apiUrl = "http://localhost:3000/books";
+
+// get book details
+function getBookDetails(id) {
+  fetch(`${apiUrl}/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("book-name").innerText = data.name;
+      document.getElementById("book-image").src = data.image_url;
+      document.getElementById("book-description").innerHTML = data.description;
+      document.getElementById("review-list").innerHTML = data.reviews
+        .map(
+          (review) => `<li>
+            ${review}
+            </li>`
+        )
+        .join("");
+    });
+}
+
+// get all books
+function getBooks() {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("book-list").innerHTML = data
+        .map(
+          (beer) => `<li onClick="getBearDetails(${book.id})">${book.name}</li>`
+        )
+        .join("");
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  getBooks();
+  getBookDetails(1);
+
+  // add a new review on submission
+  document.getElementById("review-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.getElementById("review-list").innerHTML += `<li>
+    ${document.getElementById("review").value}
+    </li>`;
+  });
+
+  // update description
+  document
+    .getElementById("description-form")
+    .addEventListener("submit", (evt) => {
+      evt.preventDefault();
+
+      form.reset();
+    });
+});
